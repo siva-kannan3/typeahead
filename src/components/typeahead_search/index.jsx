45c7e109ea1e52ac116a94a2b288a338d9e8e1e5
sivak <sivakwalker3@gmail.com>
2022-03-04 07:26:08 +0530

    update: 🚧 search list implemented with mock data

--- src/components/typeahead_search/index.jsx
+++ src/components/typeahead_search/index.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useRef, useState } from "react";
 import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch, faClose } from "@fortawesome/free-solid-svg-icons";
+import { faSearch, faClose, faStar } from "@fortawesome/free-solid-svg-icons";
+
+import MOCK_DATA from "./mock.data.json";
 
 import "./typeahead.css";
 
@@ -31,7 +33,6 @@ export const TypeAhead = ({ showCloseButton }) => {
       <div
         className="search-container"
         id="search-container"
-        ref={searchInputRef}
         tabIndex="-1"
         onFocus={() => {
           setShowOverlay(true);
@@ -43,9 +44,16 @@ export const TypeAhead = ({ showCloseButton }) => {
           name="search-input"
           aria-label="search-input"
           className="search-input"
+          ref={searchInputRef}
         />
         {search && showCloseButton && (
-          <div className="icon-wrapper close">
+          <div
+            className="icon-wrapper close"
+            onClick={() => {
+              setSearch("");
+              searchInputRef.current && searchInputRef.current.focus();
+            }}
+          >
             <FontAwesomeIcon icon={faClose} className="close-icon" />
           </div>
         )}
@@ -60,15 +68,45 @@ export const TypeAhead = ({ showCloseButton }) => {
           ref={overlayRef}
           onClick={updateShowOverlay}
         >
-          {search && (
-            <div className="search-list-container" id="search-list">
-              I am search list
-            </div>
-          )}
+          {search && <SearchList searchText={search} />}
         </div>
       )}
     </div>
   );
+};
+
+const SearchList = ({ searchText }) => {
+  const { results } = MOCK_DATA;
+  return (
+    <div className="search-list-container" id="search-list">
+      {results.map((movie) => {
+        return (
+          <div key={movie.id} className="search-list-movie">
+            <div className="movie-image-wrapper">
+              <img
+                src={`https://image.tmdb.org/t/p/w185/${movie.poster_path}`}
+                alt={movie.original_title}
+              />
+            </div>
+            <div className="movie-context">
+              <span className="movie-title">{movie.original_title}</span>
+              <span className="movie-overview">{movie.overview}</span>
+            </div>
+            <div className="movie-rating-wrapper">
+              <div className="movie-rating">
+                <FontAwesomeIcon icon={faStar} className="star-icon" />
+                <span>{movie.vote_average}</span>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+SearchList.propTypes = {
+  searchText: PropTypes.string,
 };
 
 TypeAhead.propTypes = {
